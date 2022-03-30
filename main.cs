ca168b3a66afdf31c4fa084dbb02cdf672c938aa
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-30 13:01:13 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+// Created by: Ava Venturino
+// Created on: March 22 2022
+//
+// This program accepts user input
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int baseA;
+        int baseB;
+        int height;
+
+        Console.WriteLine("This program finds the area of a trapezoid.");
+        Console.WriteLine("");
+        Console.WriteLine("Formula = (( base a + base b) / 2) x height.");
+        Console.WriteLine("");
+        Console.Write("Enter the base a of the trapezoid (mm): ");
+        baseA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the base b of the trapezoid (mm): ");
+        baseB = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the height of the triangle (mm): ");
+        height = Convert.ToInt32(Console.ReadLine());
+
+        Double area = ((baseA + baseB) / 2) * height;
+
+        Console.WriteLine("");
+        Console.Write("Area of the trapezoid = " + area + " mm²");
+        Console.ReadKey();
+        Console.WriteLine("\nDone.");
+    }
+}
